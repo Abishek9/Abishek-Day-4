abf83fd11fd73b3bf8b002c1132529958164eb90
Abishek9 <61542313+Abishek9@users.noreply.github.com>
2020-04-13 16:38:10 +0530

    Add files via upload

---
+++ java/com/capgemini/springdemo/mvc/Student.java
@@ -0,0 +1,66 @@
+package com.capgemini.springdemo.mvc;
+
+import java.util.LinkedHashMap;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private String country;
+	private String favouriteLanguage;
+	private String operatingSystem;
+	
+	public String getOperatingSystem() {
+		return operatingSystem;
+	}
+
+	public void setOperatingSystem(String operatingSystem) {
+		this.operatingSystem = operatingSystem;
+	}
+
+	public String getFavouriteLanguage() {
+		return favouriteLanguage;
+	}
+
+	public void setFavouriteLanguage(String favouriteLanguage) {
+		this.favouriteLanguage = favouriteLanguage;
+	}
+
+	private LinkedHashMap<String,String> countryOptions;
+	
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public Student() {
+		countryOptions=new LinkedHashMap<>();
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+		countryOptions.put("US", "United States of America");
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
---
+++ view/student-confirmation.jsp
@@ -0,0 +1,23 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Student Registration Form</title>
+</head>
+<body>
+	The student is confirmed: ${student.firstName} ${ student.lastName }
+	<br>
+	<br> Country:${student.country}
+	<br>
+	<br> Favourite Language:${student.favouriteLanguage}
+	<br>
+	<br> Operating System:
+	<ul>
+
+		<c:forEach var="temp" items="${student.operatingSystem} ">
+			<li>${temp}</li>
+		</c:forEach>
+	</ul>
+</body>
+</html>
